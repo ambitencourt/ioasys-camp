512df9170bc58529366ef9b5555c9e13bce6c86f
ambitencourt <adriano.mirandabitencourt@gmail.com>
2022-03-01 22:31:56 -0300

    Teste Unitário no repository

---
+++ ioasys_camp_pokedex/test/app/modules/presentation/pages/home/controllers/home_controller_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pokedex_ioasys/app/modules/infra/pokemon_list_repository.dart';
+
+main() {
+  final repository = PokemonListRepository();
+  test(
+    '''Deve trazer uma lista de Pokemons ''',
+    () async {
+      final list = await repository.getPokemonList(
+          link: 'https://pokeapi.co/api/v2/pokemon?limit=15');
+      print('$list \n');
+      print(list.pokemons);
+    },
+  );
+}
